0adedf491be3706462d6f4b927a285073dfd0287
GhostyDev <152715191+drexxdv@users.noreply.github.com>
2024-04-28 20:42:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const path = require('path');
 
+// Asumiendo que tu archivo app.js se encuentra en la raíz del proyecto
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  res.render('home');
 });
 
 const port = process.env.PORT || 3000;
