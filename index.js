b9cea8232b664c7858125d319284533d221d65f2
GhostyDev <152715191+drexxdv@users.noreply.github.com>
2024-04-28 19:34:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', async (res, req) => {
-  res.send('Servidor llamando a usuario, estas ahi?')
-})
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
+
