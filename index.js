5bdc747f12baa020a61e9a37f2937484b86cc952
GhostyDev <152715191+drexxdv@users.noreply.github.com>
2024-04-30 19:37:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,17 +17,21 @@ const { GatewayIntentBits, Client } = require('discord.js');
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 
 app.get('/api/servers', (req, res) => {
-  const servers = client.guilds.cache.array().map(guild => ({
-    id: guild.id,
-    name: guild.name,
-  }));
-  res.json(servers);
+  try {
+await client.login('MTIyMDA0NTUxNDY0MTM3NTIzMg.GVBIwZ.l0i-tWyuDLMOcoJ8w_QrImgg2YvbEhRbr9Thes');
+const guilds = await client.guilds.fetch();
+const servers = guilds.map(guild => ({
+id: guild.id,
+name:.guild.name,
+}));
+res.json(servers);
+} catch (error) {
+console.error(error);
+res.status(500).json({ error: 'Internal Server Error' });
+}
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en el puerto ${port}`);
 });
-
-client.login('MTIyMDA0NTUxNDY0MTM3NTIzMg.GVBIwZ.l0i-tWyuDLMOcoJ8w_QrImgg2YvbEhRbr9Thes')
-
