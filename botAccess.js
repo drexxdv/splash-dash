c7dd224ec7db091b85b90d0e63bbe72ac9b3c65f
GhostyDev <152715191+drexxdv@users.noreply.github.com>
2024-05-17 19:35:09 +0200

    Delete botAccess.js

--- botAccess.js
+++
@@ -1,9 +0,0 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent
-  ]
-});
-
-client.login('MTIyMDA0NTUxNDY0MTM3NTIzMg.GVBIwZ.l0i-tWyuDLMOcoJ8w_QrImgg2YvbEhRbr9Thes')
