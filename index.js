82723ffc9e57deff16bebfa10ae591a4f7385344
Ghosty <152715191+ghostydv@users.noreply.github.com>
2024-06-05 20:45:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 app.get('/commands', (req, res) => { res.render('commands'); });
-app.get('/coming-soon', (req, res) => { res.send('This thing is coming soon lol') })
+app.get('/coming-soon', (req, res) => { res.send('This thing is coming soon lol') });
+app.get('/bot-private-info.json', (req, res) => { res.send('<img src="https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713" alt="reload the page">') })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
